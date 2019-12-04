e532b9ecdac804cd4c0205da04daf8c001b7bd24
Bhuminan <dhinbhum@gmail.com>
2019-12-04 07:09:40 +0700

    update

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ var test = "http://localhost:8080/appstore";
 $(function () {
 
     $.get(test, function (data) {
-        for (var x = 0; x < 6; x++) {
+        for (var x = 0; x < 9; x++) {
             var row = ` 
 
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="image/${[x]}.jpg" alt=""></a>
+                    <a href="#"><img class="card-img-top" src="https://www.elsetge.cat/myimg/f/11-115406_wide-hd-gray.jpg" alt=""></a>
                     <div class="card-body">
                         <h4 class="card-title">
                             <a href="#">${data[x].app}</a>
